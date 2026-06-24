50b0cce32535701741921c73fa7be4b2aed60b18
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-06-24 21:17:29 +0530

    fix project controller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middleware/authMiddleware");
-const { registerUser, loginUser } = require("../controllers/authController");
 
+const {
+registerUser,
+loginUser,
+} = require("../controllers/authController");
 
-router.post("/", protect, createTask);
-router.post("/", protect, createProject);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
