e5d9548d3c3bee770e1633dbcc5d796c36c3f23c
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-06-25 01:54:30 +0530

    fix project routes and controller

--- backend/models/Subscription.js
+++ backend/models/Subscription.js
@@ -1,32 +1,37 @@
 const mongoose = require("mongoose");
 
 const subscriptionSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+{
+user: {
+type: mongoose.Schema.Types.ObjectId,
+ref: "User",
+required: true,
+},
 
-    plan: {
-      type: String,
-      enum: ["free", "pro", "team"],
-      default: "free",
-    },
 
-    status: {
-      type: String,
-      enum: ["active", "inactive", "canceled"],
-      default: "inactive",
-    },
+plan: {
+  type: String,
+  default: "Free",
+},
 
-    stripeCustomerId: String,
-    stripeSubscriptionId: String,
-  },
-  { timestamps: true, }
+status: {
+  type: String,
+  enum: ["active", "inactive", "cancelled"],
+  default: "inactive",
+},
+
+stripeCustomerId: String,
+
+stripeSubscriptionId: String,
+
+
+},
+{
+timestamps: true,
+}
 );
 
 module.exports = mongoose.model(
-  "Subscription",
-  subscriptionSchema
+"Subscription",
+subscriptionSchema
 );
