2d356bc5288bf94beb1f19fd8baa601b63ec22d4
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-06-26 02:17:18 +0530

    Fix Netlify SPA routing

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -100,6 +100,13 @@ const verifyPayment = async (req, res) => {
   }
 };
 
+
+//Razorpay
+const razorpay = new Razorpay({
+key_id: process.env.RAZORPAY_KEY_ID,
+key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
 module.exports = {
   createOrder,
   verifyPayment,
