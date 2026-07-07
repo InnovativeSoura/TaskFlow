56cd79f1c3feffae510e8276b46ea62d6eff3b35
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-07-07 14:56:33 +0530

    Fix Mongoose pre-save hook

---
+++ frontend/src/services/notificationService.js
@@ -0,0 +1,29 @@
+import api from "./api";
+
+/**
+ * Get all notifications
+ */
+export const getNotifications = async () => {
+  return await api.get("/notifications");
+};
+
+/**
+ * Mark one notification as read
+ */
+export const markNotificationAsRead = async (id) => {
+  return await api.put(`/notifications/${id}/read`);
+};
+
+/**
+ * Mark all notifications as read
+ */
+export const markAllNotificationsAsRead = async () => {
+  return await api.put("/notifications/read-all");
+};
+
+/**
+ * Delete a notification
+ */
+export const deleteNotification = async (id) => {
+  return await api.delete(`/notifications/${id}`);
+};
