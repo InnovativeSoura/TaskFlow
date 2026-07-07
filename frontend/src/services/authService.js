48a9a82db60805bb027866005b326dc5dc5afed9
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-07-08 02:05:41 +0530

    Fix Mongoose pre-save hook

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,10 +1,11 @@
+import api from "./api";
 import API from "../utils/axiosConfig";
 
 export const loginUser = (data) =>
-  API.post("/auth/login", data);
+  api.post("/auth/login", data);
 
 export const registerUser = (data) =>
-  API.post("/auth/register", data);
+  api.post("/auth/register", data);
 
 export const getCurrentUser = () =>
-  API.get("/auth/me");
+  api.get("/auth/me");
