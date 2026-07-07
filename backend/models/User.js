6acd5a0ed9813e7fae9375e18f24421fdf2a9680
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-07-07 14:37:39 +0530

    Fix Mongoose pre-save hook

--- backend/models/User.js
+++ backend/models/User.js
@@ -93,33 +93,20 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
 
-  this.password = await bcrypt.hash(
-    this.password,
-    salt
-  );
-
-  next();
-
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password
-userSchema.methods.matchPassword =
-  async function (enteredPassword) {
-
-    return await bcrypt.compare(
-      enteredPassword,
-      this.password
-    );
-
-  };
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model(
   "User",
