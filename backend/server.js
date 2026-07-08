91a7d8e5599c2441fd4fbd8fec24c164b21cee7d
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-07-08 14:34:22 +0530

    Fix Mongoose pre-save hook

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
+import settingsRoutes from "./routes/settingsRoutes.js";
 
 
 /* ==========================================
@@ -51,10 +53,17 @@ app.use(
 API ROUTES
 ========================================== */
 
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/projects", projectRoutes);
-// app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use("/api/users", userRoutes);
+
+app.use("/api/projects", projectRoutes);
+
+app.use("/api/tasks", taskRoutes);
+
+app.use("/api/notifications", notificationRoutes);
+
+app.use("/api/settings", settingsRoutes);
 
 /* ==========================================
 ROOT ROUTE
