2804b5bd0353f9a7967d3ae0ec84e3638d3009fa
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-07-11 02:24:45 +0530

    Fix authentication, notifications, dashboard and settings

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,9 +1,17 @@
 import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_SOCKET_URL, {
-  transports: ["websocket", "polling"],
   withCredentials: true,
+  transports: ["websocket", "polling"],
   autoConnect: false,
+});
+
+socket.on("connect", () => {
+  console.log("✅ Socket Connected:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.error("⚠️ Socket Error:", err.message);
 });
 
 export default socket;
