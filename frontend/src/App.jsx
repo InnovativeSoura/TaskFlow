b89094f7e30474ecd3f953a3c3a075652fbfa399
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-07-14 01:52:35 +0530

    frontend fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,7 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import LandingPage from "./pages/LandingPage";
-import AuthPage from "./pages/AuthPage";
-import Dashboard from "./pages/Dashboard";
-import PublicRoute from "./components/PublicRoute";
-import ProtectedRoute from "./components/ProtectedRoute";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <Routes>
-      {/* Landing Page */}
-      <Route
-        path="/"
-        element={<LandingPage />}
-      />
-
-      {/* Authentication */}
-      <Route
-        path="/login"
-        element={<PublicRoute>
-          <AuthPage />
-        </PublicRoute>}
-      />
-
-      {/* Dashboard */}
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-
-      {/* Redirect */}
-      <Route
-        path="*"
-        element={<Navigate to="/" replace />}
-      />
-    </Routes>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
