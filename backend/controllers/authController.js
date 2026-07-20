ce3f039457186c2be2ec02bc15b2fbd2f00ca12d
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-07-21 02:24:08 +0530

    Fix backend & frontend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,5 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
 
 /* ======================================================
    FORMAT USER RESPONSE
