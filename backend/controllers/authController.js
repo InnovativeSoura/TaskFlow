cc7ea0281bacea5d1217d501e67a78edccf9bce3
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-07-25 02:40:51 +0530

    Export generateToken from authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -28,15 +28,13 @@ const formatUser = (user) => ({
    GENERATE JWT TOKEN
 ====================================================== */
 
-const generateToken = (userId) => {
+export const generateToken = (userId) => {
   if (!process.env.JWT_SECRET) {
     throw new Error("JWT_SECRET is missing.");
   }
 
   return jwt.sign(
-    {
-      id: userId,
-    },
+    { id: userId },
     process.env.JWT_SECRET,
     {
       expiresIn: "7d",
