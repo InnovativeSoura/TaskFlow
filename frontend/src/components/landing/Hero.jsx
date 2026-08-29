ec141ab98262668d60ea47d311499f7281fa4b17
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-08-30 02:28:26 +0530

    make Users page  more premium

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -1,6 +1,6 @@
 // src/components/landing/Hero.jsx
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 
 import {
