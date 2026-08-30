bc72eab6483b7bdbcfd0d8253faab4b3ba0df002
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-08-31 02:19:22 +0530

    make Reports page  more premium

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -38,11 +38,11 @@ import {
 
 import { toast } from "react-toastify";
 
-import MainLayout from "../layouts/MainLayout";
-
 import { useProjects } from "../context/ProjectContext";
 
 import "../styles/Projects.css";
+
+import "../layouts/MainLayout";
 
 
 /* ==========================================================
